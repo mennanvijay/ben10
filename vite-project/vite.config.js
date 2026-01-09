245c58ae0b4a17d8b20cd4c3212dae493f283290
mennanvijay <mennanvijay007@gmail.com>
2026-01-09 15:56:05 +0530

    Update vite.config.js

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // <--- ADD THIS LINE: This ensures paths work on any server
 })
